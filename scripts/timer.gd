bbb1d90d4ef61f7414987db92efa766c8cb6ff8c
Philippe.H <39488884+DevShimi92@users.noreply.github.com>
2025-02-22 19:14:48 +0100

    Fix timer

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -1,30 +1,23 @@
 extends Control
 
-@export var duration = GameManager.timer_hud
+signal on_timeout
+
+@export var duration = 10
 
 @onready var current_duration = duration
 @onready var max_progress = size.x
 
 var is_event_fired = false
-var on_timeout : bool = false
+var is_playing = true
 
 func _physics_process(delta: float) -> void:
-	GameManager.timer_timeout = false
-	if (!GameManager.show_timer_hud):
+	if (!is_playing):
 		return
 	current_duration -= delta
 	size.x = lerpf(0.0, max_progress, current_duration / duration)
-	if (size.x == 0.0 && !is_event_fired && GameManager.show_timer_hud):
+	if (size.x == 0.0 && !is_event_fired && is_playing):
+		on_timeout.emit()
 		is_event_fired = true
-		on_timeout = true
-		self.queue_free()
-	if !GameManager.timer_lock:
-		GameManager.show_timer_hud = false
-		self.queue_free()
 
-func _on_tree_exiting():
-	if on_timeout :
-		GameManager.timer_timeout = true
-		print('time out')
-	GameManager.show_timer_hud = false
-	GameManager.timer_lock = false
+func stop():
+	is_playing = false
