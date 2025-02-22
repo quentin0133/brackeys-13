38f2c610d06c9e023b43e5a639a699d2ed55a163
arnaud <carrascosarnaud@gmail.com>
2025-02-22 13:44:34 +0100

    Yanis code sous tise, je tisse sous COD, nous ne sommes pas les mêmes

--- scripts/mini-games/waky-waky/waky-waky.gd
+++ scripts/mini-games/waky-waky/waky-waky.gd
@@ -48,10 +48,12 @@ func _on_nikoumouk_body_entered(body: Node2D) -> void:
 func win():
 	stop()
 	$CanvasLayer/Victory.modulate.a = 1
+	$CanvasLayer/IndicationText.visible = false
 	
 func game_over():
 	stop()
 	$CanvasLayer/GameOver.modulate.a = 1
+	$CanvasLayer/IndicationText.visible = false
 
 func stop():
 	gm.playing = false
@@ -67,4 +69,11 @@ func _on_timer_on_timeout() -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	#BUG : Crash si tu touche a 00.1 sec
 	print("ouch")
+	$FlashDmg.visible = true
+	$FlashDmg.modulate.a = 1.0
+	var tween = create_tween()
+	tween.tween_property($FlashDmg, "modulate:a", 0.0, 0.5)
+	await tween.finished
+	$FlashDmg.visible = false
+
 	timer.current_duration -= 10
