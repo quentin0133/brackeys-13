5cabdaa1d258ede2d9c966645f2db5145a38fee3
Quentin Yahia <quentin.yahia.pro@gmail.com>
2025-02-23 20:20:31 +0100

    Fix a lot of things 2

--- scripts/mini-games/journalope/loop_background.gd
+++ scripts/mini-games/journalope/loop_background.gd
@@ -6,13 +6,14 @@ extends Node2D
 
 var last_background
 var current_background = 0
+var pos_original = global_position
 
 func _ready() -> void:
 	last_background = grounds.size() - 1
-"res://scripts/AnimatedTextureRect.gd"
+
 func _physics_process(delta: float) -> void:
 	global_position.x -= delta * speed
-	GameManager.data_game["scene_journalope"][6] = absi(-global_position.x)
+	GameManager.data_game["scene_journalope"][6] = absi(-global_position.x + pos_original.x)
 	if (grounds[current_background].global_position.x <= -130):
 		grounds[current_background].global_position.x = grounds[last_background].global_position.x + 66
 		last_background = current_background
