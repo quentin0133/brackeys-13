4bb4eed6f225bf67d57a007dd4944777cb837f60
arnaud <carrascosarnaud@gmail.com>
2025-02-23 01:10:54 +0100

    rompichent

--- scenes/1_cinematic.gd
+++ scenes/1_cinematic.gd
@@ -14,18 +14,19 @@ func _input(event):
 		current_image_index += 1
 		if current_image_index < cinematics.size():
 			texture = cinematics[current_image_index]
+			match current_image_index:
+				1:
+					$"1cinematic2".play()
+				2:
+					$"1cinematic3".play()
+				3:
+					$"1cinematic4".play()
+				4:
+					$"1cinematic5".play()
+				5:
+					$"1cinematic6".play()
+				6:
+					$"1cinematic7".play()
 		else:
 			queue_free()
-	match current_image_index:
-		1:
-			$"1cinematic2".play()
-		2:
-			$"1cinematic3".play()
-		3:
-			$"1cinematic4".play()
-		4:
-			$"1cinematic5".play()
-		5:
-			$"1cinematic6".play()
-		6:
-			$"1cinematic7".play()
+	
