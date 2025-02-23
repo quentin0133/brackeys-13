3f23e4bce2eac158b96c950116b705abdcbeaa9b
Quentin Yahia <quentin.yahia.pro@gmail.com>
2025-02-23 11:14:50 +0100

    aaaaaaa

--- autoloads/GameManager.gd
+++ autoloads/GameManager.gd
@@ -13,12 +13,12 @@ var show_timer_hud : bool = false
 var timer_lock : bool = false
 
 var data_game = {
-	"scene_journalope" : ['Journalope','Description',0,'Best sprint','Last sprint',0,0,true],
+	"scene_journalope" : ['The \'Journalope\'','Run and hide from journalists',0,'Best sprint','Last sprint',0,0,true],
 	"scene_crowd_kid" : ['Crow and Kid','Description',1,'','Last time','',time_score,true],
-	"scene_wakywaky" : ['Waky waky','Description',2,'','',0,0,false],
-	"scene_grand_mother" : ['Grand Mother Game','Description',3,'','Last time','',time_score,true],
-	"scene_dont_answer" : ['Father Fouras','Description',4,'','','','',false],
-	"scene_monologue" : ['Monologue','Description',5,'','',0,0,false]
+	"scene_wakywaky" : ['Waky waky','Cross the crowd and tackle the kid',2,'','',0,0,false],
+	"scene_grand_mother" : ['Grand Mother Game','Looking for Granny\'s medicine',3,'','Last time','',time_score,true],
+	"scene_dont_answer" : ['Father Fouras','Ignore it and run away',4,'','','','',false],
+	"scene_monologue" : ['Boring monologue','Dodging boring nerd monologues',5,'','',0,0,false]
 	#"main_menu" : ['Default title','Default description','Score hud','Best Score','Last Score',0,0,false]
 }
 
