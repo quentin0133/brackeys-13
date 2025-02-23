7b53f3b3e8f4701dd21daf3f644ef1ac0912da35
Philippe.H <39488884+DevShimi92@users.noreply.github.com>
2025-02-23 01:51:04 +0100

    Add monologue game

--- scenes/minigames/monologue-dodging/monologue_dodging_game.gd
+++ scenes/minigames/monologue-dodging/monologue_dodging_game.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
+@onready var victory_UI: Control = $CanvasLayer/Victory as Control
+@onready var lose_UI: Control = $CanvasLayer/Lose as Control
+
 @export var attack_scenes: Array[PackedScene]
+
+var end_game_flag : bool = false
+var flag_lose : bool = false
+signal end_game
 
 func _ready():
 	$AttackSpawnerTimer.start()
@@ -11,3 +18,32 @@ func _on_attack_spawner_timer_timeout() -> void:
 		var attack_instance = attack_scene.instantiate()
 		add_child(attack_instance)
 		$Monologue.play()
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and end_game_flag and event.pressed:
+
+		if!GameManager.story_mode :
+			GameManager.next_scene_to_call = "main_menu"
+			GameManager.update_score("scene_journalope")
+		elif GameManager.story_mode and flag_lose:
+			GameManager.next_scene_to_call = "scene_monologue"
+		else:
+			GameManager.next_scene_to_call = "main_menu"
+		queue_free()
+
+
+func lose():
+	GameManager.timer_lock = false
+	while (lose_UI.modulate.a < 1):
+		await get_tree().create_timer(0.005).timeout
+		lose_UI.modulate.a += 0.01
+	end_game_flag = true
+	flag_lose = true
+
+
+func win():
+	GameManager.timer_lock = false
+	while (victory_UI.modulate.a < 1):
+		await get_tree().create_timer(0.005).timeout
+		victory_UI.modulate.a += 0.01
+	end_game_flag = true
--- scripts/player_grid_snap.gd
+++ scripts/player_grid_snap.gd
@@ -6,8 +6,17 @@ var grid_positions = [Vector2(0,0), Vector2(1,0), Vector2(2,0),
 var current_index = 4 # Centre
 var HP := 3
 
+signal win
+signal lose
+
 func _ready():
+	GameManager.timer_hud = 15
+	GameManager.show_timer_hud = true
 	global_position = grid_positions[4] * 100
+	
+func _process(_delta):
+	if GameManager.timer_timeout :
+		win.emit()
 
 func _input(event):
 	if event.is_action_pressed("Left") and current_index % 3 > 0:
@@ -24,10 +33,9 @@ func _input(event):
 func takeDmg():
 	HP -= 1
 	if HP <= 0:
-		print("INSERER LOGIQUE DE GAME OVER")
 		$"../Health".visible = false
 		AudioManager.defeat_sound.play()
-	
+		lose.emit()
 	$"../FlashDmg".visible = true
 	$"../FlashDmg".modulate.a = 1.0
 	$"../Health".hframes += 1
